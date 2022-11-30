e13f97353726cbfc9bbbe05570d8019e9190c3b1
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-11-30 13:05:42 +0100

    gamecommit

--- transversal_g1/app/Http/Controllers/GameController.php
+++ transversal_g1/app/Http/Controllers/GameController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class GameControlloer extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $game= new game;
+
+        $user->id=$request->id;
+        $user->data=$request->data;
+        $user->difficulty=$request->difficulty;
+        $user->json=$request->json;
+        $user->iduser=$request->iduser;
+        $user->store();
+
+        //
+    }
 }
