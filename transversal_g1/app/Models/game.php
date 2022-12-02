7a649cc359a75410f67bb56f58f87fd7c7a2acb5
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-02 10:17:42 +0100

    uwu3

--- transversal_g1/app/Models/game.php
+++ transversal_g1/app/Models/game.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Http\Controllers\GameController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
--- transversal_g1/routes/api.php
+++ transversal_g1/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\GameController;
+use App\Http\Controllers\GameController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('store-game', [GameController::class, 'store']);
+Route::post('/store-game', [GameController::class, 'store']);
