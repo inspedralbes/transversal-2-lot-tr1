58dfca1557e98ab6ad2ad6054489281d917d6ec4
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-05 13:23:05 +0100

    ssss

--- transversal_g1/routes/api.php
+++ transversal_g1/routes/api.php
@@ -20,3 +20,4 @@ use App\Http\Controllers\UserController;
 
 Route::post('/store-game', [GameController::class , 'store']);
 Route::post('/register-user',[UserController::class, 'store']);
+Route::post('/login',[UserController::class, 'login']);
