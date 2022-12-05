f225257ad7627456c3c3e83c3ac98919dccd6c2d
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-05 13:47:06 +0100

    xd

--- transversal_g1/routes/api.php
+++ transversal_g1/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\PuntuacioController;
 use App\Http\Controllers\UserController;
 
 /*
