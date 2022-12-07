f2a5f5144f20655560f78bef9094b5294c665e55
Marti-SV <a21marsalval@inspedralbes.cat>
2022-12-07 08:53:27 +0100

    Prova funcionalitat login

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,5 @@
 Vue.component("register", {
-  template: `<div class="flex flex-wrap w-full justify-center items-center pt-56">
+    template: `<div class="flex flex-wrap w-full justify-center items-center pt-56">
     <div class="flex flex-wrap max-w-xl">
         <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
         <div class="p-2 w-full">
@@ -24,83 +24,57 @@ Vue.component("register", {
         </div>
     </div> 
 </div>`,
-  data() {
-    return {
-      form: {
-        name: "",
-        email: "",
-        password: "",
-        descripcio: "",
-      },
-      errors: [],
-    };
-  },
-  methods: {
-    saveForm() {
-      fetch("../transversal_g1/public/api/register-user", {
-        method: "POST",
-        body: this.form,
-      }).then(() => {
-        console.log("saved");
-      });
+    data() {
+        return {
+            form: {
+                name: "",
+                email: "",
+                password: "",
+                descripcio: ""
+            },
+            errors: []
+        };
     },
-  },
+    methods: {
+        saveForm() {
+            fetch("../transversal_g1/public/api/register-user", {
+                method: "POST",
+                body: this.form
+            }).then(() => {
+                console.log("saved");
+            });
+        }
+    }
 });
 Vue.component("login", {
-  template: `<div>
-    <div v-show="!logged">
-    <b-form-input v-model="form.username" placeholder="Username" required></b-form-input>
-    <b-form-input v-model="form.password" placeholder="Password" required></b-form-input>
+    template: `<div>
+    <b-form-input v-model="form.email" placeholder="Correu electrònic" required></b-form-input>
+    <b-form-input v-model="form.password" placeholder="Contrasenya" required></b-form-input>
     <b-button @click="submitLogin" variant="primary">Login</b-button>
-    <div v-show="processing">
-        <b-spinner></b-spinner>
-    </div>
-    </div>
-    <div v-show="logged">
-    Benvingut {{nameUser}} <img :src=imageUser> 
-    <b-button @click="logOut" variant="primary">Logout</b-button>
-    </div>
     </div>`,
-  data: function () {
-    return {
-      nameUser: "",
-      imageUser: "",
-      form: {
-        username: "",
-        password: "",
-      },
-      logged: false,
-      processing: false,
-    };
-  },
-  methods: {
-    submitLogin() {
-      this.processing = true;
-      fetch(
-        `http://alvaro.alumnes.inspedralbes.cat/loginGET.php?username=${this.form.username}&pwd=${this.form.password}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.exito) {
-            this.nameUser = data.nombre;
-            this.imageUser = data.imagen;
-            this.logged = true;
+    data: function () {
+        return {
+            form: {
+                email: "",
+                password: ""
+            },
+        };
+    },
+    methods: {
+        submitLogin() {
 
-            store = userStore();
-            store.setEstado(this.infoLogin);
-            store.logged = true;
-          }
-        });
-    },
-    logOut() {
-      this.logged = false;
-      this.processing = false;
-    },
-  },
+            fetch("../transversal_g1/public/api/login", {
+                method: "POST",
+                body: this.form
+            }).then(() => {
+                console.log("login");
+            });
+        }
+    }
 });
 
 Vue.component("navbar", {
-  template: `<div>
+    template: `<div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.html"><img src="img/logo_omg_navbar.png" alt="Logo" style="width: 5vw;"></a>
@@ -143,41 +117,40 @@ Vue.component("navbar", {
     </div>
   </b-modal>
 </div>`,
-  data: function () {
-    return {};
-  },
-  methods: {},
+    data: function () {
+        return {};
+    },
+    methods: {}
 });
 
 const home = Vue.component("home", {
-  template: `<div>
+    template: `<div>
     <navbar></navbar>
-    <login></login>
     <h1>TRIVIAL</h1>
     <button ><router-link to="/partida">Jugar</router-link></button>
     </div>`,
-  data: function () {
-    return {};
-  },
-  methods: {},
+    data: function () {
+        return {};
+    },
+    methods: {}
 });
 
 const partida = Vue.component("opcions", {
-  data: function () {
-    return {
-      categoria: "",
-      dificultat: "",
-      preguntesRespostes: [],
-      opcionsTriades: false,
-      preguntaActual: 0,
-      dadesPartida: {
-        punts: 0,
-        tempsPartida: 0,
-        acabada: false,
-      },
-    };
-  },
-  template: `<div>
+    data: function () {
+        return {
+            categoria: "",
+            dificultat: "",
+            preguntesRespostes: [],
+            opcionsTriades: false,
+            preguntaActual: 0,
+            dadesPartida: {
+                punts: 0,
+                tempsPartida: 0,
+                acabada: false
+            }
+        };
+    },
+    template: `<div>
 
  <navbar></navbar>
     <div v-show="!opcionsTriades">
@@ -210,77 +183,64 @@ const partida = Vue.component("opcions", {
     </div>
     </div>
     </div>`,
-  methods: {
-    buscarQuiz: async function () {
-      await fetch(
-        "https://the-trivia-api.com/api/questions?categories=" +
-          this.categoria +
-          "&limit=10&difficulty=" +
-          this.dificultat
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          this.preguntesRespostes = data;
-        });
-      this.opcionsTriades = true;
-    },
-    addGame: function () {
-      const enviar = new FormData();
-      var numDificultat = 0;
-      switch (this.preguntesRespostes[0].difficulty) {
-        case "easy":
-          numDificultat = 1;
-          break;
-        case "medium":
-          numDificultat = 2;
-          break;
-        case "hard":
-          numDificultat = 3;
-          break;
-      }
-      enviar.append("difficulty", numDificultat);
-      enviar.append("category", this.preguntesRespostes[0].category);
-      enviar.append("json", JSON.stringify(this.preguntesRespostes));
+    methods: {
+        buscarQuiz: async function () {
+            await fetch("https://the-trivia-api.com/api/questions?categories=" + this.categoria + "&limit=10&difficulty=" + this.dificultat).then((response) => response.json()).then((data) => {
+                this.preguntesRespostes = data;
+            });
+            this.opcionsTriades = true;
+        },
+        addGame: function () {
+            const enviar = new FormData();
+            var numDificultat = 0;
+            switch (this.preguntesRespostes[0].difficulty) {
+                case "easy": numDificultat = 1;
+                    break;
+                case "medium": numDificultat = 2;
+                    break;
+                case "hard": numDificultat = 3;
+                    break;
+            }
+            enviar.append("difficulty", numDificultat);
+            enviar.append("category", this.preguntesRespostes[0].category);
+            enviar.append("json", JSON.stringify(this.preguntesRespostes));
 
-      fetch("../transversal_g1/public/api/store-game", {
-        method: "POST",
-        body: enviar,
-      });
-    },
-  },
+            fetch("../transversal_g1/public/api/store-game", {
+                method: "POST",
+                body: enviar
+            });
+        }
+    }
 });
 
 Vue.component("pregunta", {
-  props: ["infoPreguntes", "index", "estatP"],
-  data: function () {
-    return {
-      respostesOrdenades: [],
-      respostesDesordenades: [],
-      respostaContestada: false,
-      segons: 20,
-      b0: "",
-      b1: "",
-      b2: "",
-      b3: "",
-    };
-  },
-  mounted() {
-    this.respostesOrdenades = [
-      this.infoPreguntes.correctAnswer,
-      this.infoPreguntes.incorrectAnswers[0],
-      this.infoPreguntes.incorrectAnswers[1],
-      this.infoPreguntes.incorrectAnswers[2],
-    ];
-    this.respostesDesordenades = this.respostesOrdenades;
+    props: [
+        "infoPreguntes", "index", "estatP"
+    ],
+    data: function () {
+        return {
+            respostesOrdenades: [],
+            respostesDesordenades: [],
+            respostaContestada: false,
+            segons: 20,
+            b0: "",
+            b1: "",
+            b2: "",
+            b3: ""
+        };
+    },
+    mounted() {
+        this.respostesOrdenades = [this.infoPreguntes.correctAnswer, this.infoPreguntes.incorrectAnswers[0], this.infoPreguntes.incorrectAnswers[1], this.infoPreguntes.incorrectAnswers[2],];
+        this.respostesDesordenades = this.respostesOrdenades;
 
-    for (let i = this.respostesDesordenades.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = this.respostesDesordenades[i];
-      this.respostesDesordenades[i] = this.respostesDesordenades[j];
-      this.respostesDesordenades[j] = temp;
-    }
-  },
-  template: `<div>
+        for (let i = this.respostesDesordenades.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.respostesDesordenades[i];
+            this.respostesDesordenades[i] = this.respostesDesordenades[j];
+            this.respostesDesordenades[j] = temp;
+        }
+    },
+    template: `<div>
     <h1>{{ segons }}</h1>
     <h1>{{ infoPreguntes.question }}</h1>
     <b-button :variant="b0" @click="respostaCorrecte(0)">{{ respostesDesordenades[0] }}</b-button>
@@ -288,92 +248,86 @@ Vue.component("pregunta", {
     <b-button :variant="b2" @click="respostaCorrecte(2)">{{ respostesDesordenades[2] }}</b-button>
     <b-button :variant="b3" @click="respostaCorrecte(3)">{{ respostesDesordenades[3] }}</b-button>
    </div>`,
-  methods: {
-    respostaCorrecte: function (nRes) {
-      this.respostaContestada = true;
-      if (
-        this.respostesDesordenades[nRes] == this.infoPreguntes.correctAnswer
-      ) {
-        this.buttonColors("success", nRes);
-        setTimeout(() => {
-          this.$emit("sumaPunts");
-          this.$emit("next-question");
-        }, 2000);
-        for (let i = 0; i < 4; i++) {
-          if (i != nRes) {
-            this.buttonColors("danger", i);
-          }
+    methods: {
+        respostaCorrecte: function (nRes) {
+            this.respostaContestada = true;
+            if (this.respostesDesordenades[nRes] == this.infoPreguntes.correctAnswer) {
+                this.buttonColors("success", nRes);
+                setTimeout(() => {
+                    this.$emit("sumaPunts");
+                    this.$emit("next-question");
+                }, 2000);
+                for (let i = 0; i < 4; i++) {
+                    if (i != nRes) {
+                        this.buttonColors("danger", i);
+                    }
+                }
+            } else {
+                for (let i = 0; i < 4; i++) {
+                    if (this.respostesDesordenades[i] != this.infoPreguntes.correctAnswer) {
+                        this.buttonColors("danger", i);
+                    } else {
+                        this.buttonColors("success", i);
+                    }
+                }
+                setTimeout(() => {
+                    this.$emit("next-question");
+                }, 2000);
+            }
+        },
+        buttonColors: function (color, nRes) {
+            switch (nRes) {
+                case 0:
+                    this.b0 = color;
+                    break;
+                case 1:
+                    this.b1 = color;
+                    break;
+                case 2:
+                    this.b2 = color;
+                    break;
+                case 3:
+                    this.b3 = color;
+                    break;
+            }
+        },
+        countDownTimer() {
+            if (this.segons > 0 && this.respostaContestada == false) {
+                setTimeout(() => {
+                    this.segons -= 1;
+                    if (this.respostaContestada == true) {
+                        this.segons += 1;
+                        this.$emit("sumarTemps", (this.segons - 20) * -1);
+                    }
+                    this.countDownTimer();
+                }, 1000);
+            }
+            if (this.segons == 0) {
+                this.$emit("sumarTemps", (this.segons - 20) * -1);
+                this.$emit("next-question");
+            }
         }
-      } else {
-        for (let i = 0; i < 4; i++) {
-          if (
-            this.respostesDesordenades[i] != this.infoPreguntes.correctAnswer
-          ) {
-            this.buttonColors("danger", i);
-          } else {
-            this.buttonColors("success", i);
-          }
-        }
-        setTimeout(() => {
-          this.$emit("next-question");
-        }, 2000);
-      }
     },
-    buttonColors: function (color, nRes) {
-      switch (nRes) {
-        case 0:
-          this.b0 = color;
-          break;
-        case 1:
-          this.b1 = color;
-          break;
-        case 2:
-          this.b2 = color;
-          break;
-        case 3:
-          this.b3 = color;
-          break;
-      }
-    },
-    countDownTimer() {
-      if (this.segons > 0 && this.respostaContestada == false) {
-        setTimeout(() => {
-          this.segons -= 1;
-          if (this.respostaContestada == true) {
-            this.segons += 1;
-            this.$emit("sumarTemps", (this.segons - 20) * -1);
-          }
-          this.countDownTimer();
-        }, 1000);
-      }
-      if (this.segons == 0) {
-        this.$emit("sumarTemps", (this.segons - 20) * -1);
-        this.$emit("next-question");
-      }
-    },
-  },
-  created() {
-    this.countDownTimer();
-  },
+    created() {
+        this.countDownTimer();
+    }
 });
 
 const routes = [
-  {
-    path: "/",
-    component: home,
-  },
-  {
-    path: "/partida",
-    component: partida,
-  },
+    {
+        path: "/",
+        component: home
+    }, {
+        path: "/partida",
+        component: partida
+    },
 ];
 
-const router = new VueRouter({ routes });
+const router = new VueRouter({routes});
 
 Vue.use(BootstrapVue);
 let app = new Vue({
-  el: "#app",
-  router,
-  // pinia,
-  data: {},
+    el: "#app", router,
+    // pinia,
+    data: {}
 });
