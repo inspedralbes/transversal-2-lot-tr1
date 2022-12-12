f00262bb49f51e05d0b22063ed603ea66062aece
Marti-SV <a21marsalval@inspedralbes.cat>
2022-12-12 12:11:36 +0100

    missatge usuari creat

--- web/js/app.js
+++ web/js/app.js
@@ -1,9 +1,6 @@
 const userStore = Pinia.defineStore('usuario', {
     state() {
-        return {
-            logged: false,
-            name: ''
-        }
+        return {logged: false, name: ''}
     }
 })
 
@@ -13,27 +10,33 @@ const pinia = Pinia.createPinia()
 Vue.component("register", {
     template: `<div class="flex flex-wrap w-full justify-center items-center pt-56">
     <div class="flex flex-wrap max-w-xl">
-        <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
+        <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Registra un compte</h1></div>
+    <div v-show="!creat">
         <div class="p-2 w-full">
-            <label class="w-full" for="nickname">Name</label>
+            <label class="w-full" for="nickname">Nom</label>
             <span class="w-full text-red-500" v-if="errors.nickname">{{errors.nickname[0]}}</span>
-            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" v-model="form.nickname" >
+            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Nom" type="text" v-model="form.nickname" >
         </div>
         <div class="p-2 w-full">
-            <label for="email">Your e-mail</label>
-            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Email" type="email" v-model="form.email">
+            <label for="email">El teu correu electrònic</label>
+            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Correu electrònic" type="email" v-model="form.email">
         </div>
         <div class="p-2 w-full">
-            <label for="password">Password</label>
-            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Password" type="password" v-model="form.password" name="password">
+            <label for="password">Contrasenya</label>
+            <input class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Contrasenya" type="password" v-model="form.password" name="password">
         </div>
         <div class="p-2 w-full mt-4">
-            <button @click.prevent="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+            <button @click.prevent="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Crea el compte</button>
         </div>
+        </div>
+    <div v-show="creat">
+        <h2>Usuari creat correctament!</h2>
+    </div>
     </div> 
 </div>`,
     data() {
         return {
+            creat: false,
             form: {
                 nickname: "",
                 email: "",
@@ -55,7 +58,7 @@ Vue.component("register", {
                 method: "POST",
                 body: enviar
             }).then(() => {
-                console.log("saved");
+                creat = true;
             });
         }
     }
@@ -91,7 +94,6 @@ Vue.component("login", {
                 store.name = data.nickname;
                 store.logged = true;
             }).catch(() => {
-                this.$emit("evtDadesUsuari", "hola");
                 console.error('Error:');
 
             });
@@ -152,7 +154,7 @@ Vue.component("navbar", {
     </template>
     <div class="d-block text-center">
         <div v-show="!registrar">
-            <login @evtDadesUsuari="(d) => dadesUsuari = d"></login>
+            <login></login>
             <b-button @click="registrar = true">No tens compte?</b-button>
 
         </div>
