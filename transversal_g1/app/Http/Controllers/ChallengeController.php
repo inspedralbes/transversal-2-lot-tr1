066e39289b1de20a4488c84f7d1e96b1add12168
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-18 12:32:09 +0100

    d

--- transversal_g1/app/Http/Controllers/ChallengeController.php
+++ transversal_g1/app/Http/Controllers/ChallengeController.php
@@ -22,7 +22,7 @@ class ChallengeController extends Controller
     }
     public function checkChallenge(Request $request){
         $challenged=DB::select('SELECT DISTINCT nickname AS challengedName, idChallenged FROM challenges JOIN users ON id=idChallenged WHERE id='.$request->idChallenged);
-        $challenger=DB::select('SELECT nickname, id AS challengerName, idChallenger FROM challenges JOIN users ON id=idChallenger WHERE idChallenged='.$request->idChallenged);
+        $challenger=DB::select('SELECT nickname AS challengerName, idChallenger FROM challenges JOIN users ON id=idChallenger WHERE idChallenged='.$request->idChallenged);
         return response()->json(['challenger'=>$challenger, 'challenged'=>$challenged]);
         //uwu
     }
