d7f25620d17d491eac1eebb4c30d8dc8360bcfd6
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-18 11:16:50 +0100

    fd

--- transversal_g1/app/Http/Controllers/UserController.php
+++ transversal_g1/app/Http/Controllers/UserController.php
@@ -76,6 +76,7 @@ class UserController extends Controller
         $user->save();
     }
     public function showAllUsers(Request $request){
-        DB::select('SELECT nickname FROM users WHERE NOT id='.$request->userId);
+        $users=DB::select('SELECT nickname FROM users WHERE NOT id='.$request->userId);
+        return response()->json($users);
     }
 }
