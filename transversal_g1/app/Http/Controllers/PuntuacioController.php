5d159745824143b4aefa6ac6af0049a2841451a0
OscarLealVeyeta <a21oscleagar@inspedralbes.cat>
2022-12-19 12:17:15 +0100

    fdsa

--- transversal_g1/app/Http/Controllers/PuntuacioController.php
+++ transversal_g1/app/Http/Controllers/PuntuacioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\puntuacions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class PuntuacioController extends Controller
 {
@@ -14,8 +16,9 @@ class PuntuacioController extends Controller
 
         $puntuacions->idUser=$request->idUser;
         if($request->challenge==true){
-            
+
         }
+        $puntuacions->idGame=DB::select('SELECT games.id FROM games JOIN users ON users.id='.$request->idUser.'where games.id = (SELECT MAX(games.id) FROM games');
         $puntuacions->puntuacio=$request->puntuacio;
         $puntuacions->save();
 
